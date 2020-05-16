9035c096c6700bfb6395e32f601b34fe6ffb2937
Mary Matthews <mary.matthews@formation.ai>
2020-05-15 20:37:07 -0700

    works serverless

--- server.js
+++ server.js
@@ -1,21 +1,24 @@
 const express = require('express');
 const path = require('path');
 // const cors = require('cors');
-const dotenv = require('dotenv');
-const http = require('http');
-const bodyParser = require('body-parser');
+// const dotenv = require('dotenv');
+// const http = require('http');
+// const bodyParser = require('body-parser');
 
 
 const app = express();
-
-if (development) reload = require('reload');
-else app.use(express.static(buildDir));
-
 const buildDir = path.join(__dirname, 'build');
 
-dotenv.load();
-const development = process.env.NODE_ENV === 'development';
-let reload;
+// if (development) {
+  // reload = require('reload') 
+// } else {
+  app.use(express.static(buildDir));
+// }
+
+
+// dotenv.load();
+// const development = process.env.NODE_ENV === 'development';
+// let reload;
 
 // const store = new MongoDBStore(
 //   {
@@ -27,7 +30,7 @@ let reload;
 // );
 // const db = require('./db');
 
-app.set('port', process.env.PORT || 3000);
+// app.set('port', process.env.PORT || 3000);
 // app.use(
 //   session({
 //     name: 'server-session-cookie-id',
@@ -41,31 +44,31 @@ app.set('port', process.env.PORT || 3000);
 // );
 
 
-if (process.env.NODE_ENV !== 'development') {
-  app.get('*', cors(), (req, res) => {
-    console.log(147);
+// if (process.env.NODE_ENV !== 'development') {
+  // app.get('*', cors(), (req, res) => { 
+app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
   });
-}
+// }
 
-const server = http.createServer(app);
-if (development) {
-  reload(app)
-    .then(() => {
-      server.listen(app.get('port'), () => {
-        console.log(`Web server listening on port ${app.get('port')}`);
-      });
-    })
-    .catch(err => {
-      console.error(
-        'Reload could not start, could not start server/sample app',
-        err
-      );
-    });
-} else {
-  server.listen(app.get('port'), () => {
-    console.log(`Web server listening on port ${app.get('port')}`);
-  });
-}
+// const server = http.createServer(app);
+// if (development) {
+//   reload(app)
+//     .then(() => {
+//       server.listen(app.get('port'), () => {
+//         console.log(`Web server listening on port ${app.get('port')}`);
+//       });
+//     })
+//     .catch(err => {
+//       console.error(
+//         'Reload could not start, could not start server/sample app',
+//         err
+//       );
+//     });
+// } else {
+//   server.listen(app.get('port'), () => {
+//     console.log(`Web server listening on port ${app.get('port')}`);
+//   });
+// }
 
 module.exports = app;
